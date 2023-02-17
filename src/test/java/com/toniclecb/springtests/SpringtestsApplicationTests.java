8c4e8e3c341b8c7496442431410e0e5e607bd8ca
Cleiton <toniclecb@gmail.com>
2023-02-17 19:00:54 -0300

    Testing controller creation and a sanity check over application context

--- src/test/java/com/toniclecb/springtests/SpringtestsApplicationTests.java
+++ src/test/java/com/toniclecb/springtests/SpringtestsApplicationTests.java
@@ -1,13 +1,32 @@
 package com.toniclecb.springtests;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.toniclecb.springtests.controller.PersonController;
+
+/**
+ * The @SpringBootTest annotation tells Spring Boot to look for a main configuration class 
+ * (one with @SpringBootApplication, for instance) and use that to start a Spring application context.
+ *
+ */
 @SpringBootTest
 class SpringtestsApplicationTests {
 
+	// Spring interprets the @Autowired annotation, and the controller is injected before the test methods are run.
+	@Autowired
+	PersonController personController;
+	
+	/**
+	 * Simple sanity check test that will fail if the application context cannot start.
+	 */
 	@Test
 	void contextLoads() {
+		// To convince that the context is creating the controller, you could add an assertion like that
+		assertThat(personController).isNotNull();
 	}
 
 }
